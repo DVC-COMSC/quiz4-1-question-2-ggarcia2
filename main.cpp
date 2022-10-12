941d91a7604b3a738a3d991198148386ea958bff
Gerardo56 <112590840+ggarcia2@users.noreply.github.com>
2022-10-12 23:30:42 +0000

    quiz 4

--- main.cpp
+++ main.cpp
@@ -1,20 +1,39 @@
-// Complete the following program. 
 #include <iostream>
 using namespace std;
-
 int main()
 {
-	const double rate1 = 1.10;
-	const double rate2 = 2.20;
-	const double rate3 = 3.70;
-	const double rate4 = 4.80;
-	double total_charge;
-	double weight, distance, rate;
+        const double RATE2 = 1.10;
+        const double RATE6 = 2.20;
+        const double RATE10 = 3.70;
+        const double RATE20 = 4.80;
+        double weight, distance, dRate, price;
 
-	
-        // **********************
-        // Your code 
-        // **********************
-        
-	cout << total_charge << endl;
+        cout << "Enter the package weight and distance\n";
+        cin >> weight >> distance;
+        //? Input Validation
+        if ((weight < 0) && (weight > 20))
+        {
+                cout << "The package weight must be a positive number and less than 20.\n";
+                exit(0);
+        }
+
+        if (weight < 2)
+                dRate = RATE2;
+        else if (weight < 6)
+                dRate = RATE6;
+        else if (weight < 10)
+                dRate = RATE10;
+        else if (weight <= 20)
+                dRate = RATE20;
+        else
+        {
+                cout << "The weight must be less than 20\n";
+                exit(0);
+        }
+
+        if (distance < 500)
+                price = dRate;
+        else
+                price = (distance / 500.0) * dRate;
+        cout << "The shipping price for package is " << price << endl;
 }
